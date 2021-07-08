eae597f29c7d5e9b79604daa825c5265362b21c2
Evelyn Hasama <ehasama11@gmail.com>
2021-07-08 10:12:36 -0700

    modify profile tab to show user's photo and information in a grid

--- app/src/main/java/com/example/parstagramandroid/ProfileFragment.java
+++ app/src/main/java/com/example/parstagramandroid/ProfileFragment.java
@@ -1,12 +1,29 @@
 package com.example.parstagramandroid;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +32,17 @@ import android.view.ViewGroup;
  */
 public class ProfileFragment extends Fragment {
 
+    public static final String TAG = "ProfileFragment";
+    public static final String CREATED_AT = "createdAt";
+    public static final String PARSEUSER_BIO_KEY = "bio";
+
+    RecyclerView rvUserPosts;
+    TextView tvBio;
+    TextView tvUser;
+    ImageView ivPfP;
+    View view;
+    List<Post> userPosts;
+    ProfilePostsAdapter adapter;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -34,7 +62,52 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ParseUser user = ParseUser.getCurrentUser();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        rvUserPosts = view.findViewById(R.id.rvUserPosts);
+        tvBio = view.findViewById(R.id.tvBio);
+        tvUser = view.findViewById(R.id.tvUser);
+        ivPfP = view.findViewById(R.id.ivPfP);
+        Glide.with(view).load(R.drawable.instagram_user_filled_24).circleCrop().into(ivPfP);
+
+        tvBio.setText(user.getString(PARSEUSER_BIO_KEY));
+        tvUser.setText(user.getUsername());
+
+        userPosts = new ArrayList<>();
+        adapter = new ProfilePostsAdapter(getActivity(),userPosts);
+        // set the adapter on the recycler view
+        rvUserPosts.setAdapter(adapter);
+        // set the layout manager on the recycler view
+        rvUserPosts.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        // query user's posts from Parstagram
+        queryUserPosts(user);
+
+        return view;
+    }
+
+    private void queryUserPosts(ParseUser user) {
+
+        // specify what type of data we want to query - Post.class
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.addDescendingOrder(CREATED_AT);
+        query.whereEqualTo("user", user);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                // check for errors
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                // save received posts to list and notify adapter of new data
+                userPosts.addAll(posts);
+                Log.d(TAG, "queryUserPosts size: "+ userPosts.size());
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/parstagramandroid/ProfilePostsAdapter.java
+++ app/src/main/java/com/example/parstagramandroid/ProfilePostsAdapter.java
@@ -2,7 +2,7 @@ package com.example.parstagramandroid;
 
 import android.content.Context;
 import android.content.Intent;
-import android.media.Image;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,75 +15,61 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.parse.ParseFile;
 
-import org.w3c.dom.Text;
-
 import java.util.List;
 
-public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
+public class ProfilePostsAdapter extends RecyclerView.Adapter<ProfilePostsAdapter.ViewHolder> {
 
+    public static final String TAG = "ProfilePostsAdapter";
     public static final String POST_ID_EXTRA = "postId";
     private Context context;
     private List<Post> posts;
 
-    public PostsAdapter(Context context, List<Post> posts) {
-        this.context = context;
-        this.posts = posts;
-    }
+    public ProfilePostsAdapter(Context context, List<Post> posts) {
+            this.context = context;
+            this.posts = posts;
+            }
 
     public void clear() {
-        posts.clear();
-        notifyDataSetChanged();
-    }
+            posts.clear();
+            notifyDataSetChanged();
+            }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
-        return new ViewHolder(view);
-    }
+            Log.d(TAG, "onCreateViewHolder");
+            View view = LayoutInflater.from(context).inflate(R.layout.item_profile_post, parent, false);
+            return new ViewHolder(view);
+            }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Post post = posts.get(position);
-        holder.bind(post);
-    }
+            Post post = posts.get(position);
+            holder.bind(post);
+            }
 
     @Override
     public int getItemCount() {
-        return posts.size();
-    }
+            return posts.size();
+            }
 
     class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private TextView tvUsername;
-        private ImageView ivImage;
-        private TextView tvDescription;
-        private ImageView ivProfileImage;
-        private TextView tvLikes;
+        private ImageView ivPostPicture;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            tvUsername = itemView.findViewById(R.id.tvUsername);
-            ivImage = itemView.findViewById(R.id.ivImage);
-            tvDescription = itemView.findViewById(R.id.tvDescription);
-            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
-            tvLikes = itemView.findViewById(R.id.tvLikes);
-
+            ivPostPicture = itemView.findViewById(R.id.ivPostPicture);
 
             itemView.setOnClickListener(this);
         }
 
         public void bind(Post post) {
             // Bind the post data to the view elements
-            tvDescription.setText(post.getDescription());
-            tvUsername.setText(post.getUser().getUsername());
             ParseFile image = post.getImage();
             if (image != null) {
-                Glide.with(context).load(image.getUrl()).into(ivImage);
+                Glide.with(context).load(image.getUrl()).centerCrop().into(ivPostPicture);
             }
-            tvLikes.setText(String.valueOf(post.getLikes()));
-            Glide.with(context).load(R.drawable.instagram_user_filled_24).circleCrop().into(ivProfileImage);
         }
 
         @Override
@@ -99,4 +85,6 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
             }
         }
     }
-}
+    }
+
+
